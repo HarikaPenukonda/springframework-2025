b9a22ff0621d951c172950dbc0d245bb9ccada05
HarikaPenukonda <penukondaharika@gmail.com>
2025-01-18 14:32:40 -0600

    Eliminating the application config xml file

---
+++ LoveCalculator/src/main/java/com/seleniumexpress/lc/config/LoveCalculatorAppConfig.java
@@ -0,0 +1,10 @@
+package com.seleniumexpress.lc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.seleniumexpress.lc.controller")
+public class LoveCalculatorAppConfig {
+
+}
