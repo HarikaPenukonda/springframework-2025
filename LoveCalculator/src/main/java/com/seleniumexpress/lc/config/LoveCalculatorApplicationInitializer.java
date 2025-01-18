ea3719ab42e906f2352a0666f2ae9e257ddf0a38
HarikaPenukonda <penukondaharika@gmail.com>
2025-01-18 13:59:40 -0600

    web.xml in java

---
+++ LoveCalculator/src/main/java/com/seleniumexpress/lc/config/LoveCalculatorApplicationInitializer.java
@@ -0,0 +1,32 @@
+package com.seleniumexpress.lc.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
+
+public class LoveCalculatorApplicationInitializer implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		
+		XmlWebApplicationContext webApplicationContext = new XmlWebApplicationContext();
+		webApplicationContext.setConfigLocation("classpath:application-config.xml");
+		
+		// create dispatcher servlet
+		DispatcherServlet dispatcherServlet = new DispatcherServlet();
+		
+		// register dispatcher servlet with servlet context
+		ServletRegistration.Dynamic customServlet =  servletContext.addServlet("myDispatcherServlet", dispatcherServlet);
+		
+		// setting load-on-startup
+		customServlet.setLoadOnStartup(1);
+		
+		// setting url mapping
+		customServlet.addMapping("/website.com/*");
+	}	
+
+}
