3c9c6b1f8918f1511d71b09ca215c141341e4a55
HarikaPenukonda <penukondaharika@gmail.com>
2025-01-20 17:04:24 -0600

    Valid Annotation and BindingResult Interface

--- LoveCalculator/src/main/java/com/seleniumexpress/lc/controller/LcController.java
+++ LoveCalculator/src/main/java/com/seleniumexpress/lc/controller/LcController.java
@@ -1,11 +1,14 @@
 package com.seleniumexpress.lc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.seleniumexpress.lc.api.UserInfoDTO;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class LcController {
@@ -16,7 +19,22 @@ public class LcController {
 	}
 	
 	@RequestMapping("/processhome")
-	public String processHomePage(@ModelAttribute("userInfo") UserInfoDTO userInfoDTO) {
+	public String processHomePage(@Valid @ModelAttribute("userInfo") UserInfoDTO userInfoDTO, BindingResult result) 
+	{	
+		if(result.hasErrors()) {
+			System.out.println("Form has errors");
+		}
+		
+		/*
+		 * the @Valid annotation is used to trigger validation for a bean or object
+		 * based on the constraints defined in the bean's class. It is part of the Bean
+		 * Validation API (JSR 303 and JSR 380).
+		 */
+		/*
+		 * BindingResult is an interface used to capture and manage the result of
+		 * validation. It holds the outcome of binding input data to an object and
+		 * provides access to validation errors if any constraints are violated.
+		 */
 		return "result-page";
 	}
 
