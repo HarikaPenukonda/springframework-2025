579c39f9aa528bcab04d0cdad2270f8ff99b42d8
HarikaPenukonda <penukondaharika@gmail.com>
2025-01-21 17:00:22 -0600

    logic of split and extract phone number

--- LoveCalculator/src/main/java/com/seleniumexpress/lc/formatter/PhoneNumberFormatter.java
+++ LoveCalculator/src/main/java/com/seleniumexpress/lc/formatter/PhoneNumberFormatter.java
@@ -16,9 +16,17 @@ public class PhoneNumberFormatter implements Formatter<Phone>{
 	}
 
 	@Override
-	public Phone parse(String text, Locale locale) throws ParseException {
-		// TODO Auto-generated method stub
-		return null;
+	public Phone parse(String phoneNumber, Locale locale) throws ParseException {
+		
+		// split the string recieved from the user
+		String[] phoneNumberArray = phoneNumber.split("-");
+		
+		// Extract the countrycode and set it to Phone class country code property
+		Phone phone = new Phone();
+		phone.setCountryCode(phoneNumberArray[0]);
+		phone.setUserNumber(phoneNumberArray[1]);
+		
+		return phone;
 	}
 
 }
--- LoveCalculator/src/main/webapp/WEB-INF/view/register-user-info.jsp
+++ LoveCalculator/src/main/webapp/WEB-INF/view/register-user-info.jsp
@@ -33,5 +33,6 @@
 	<h4>Gender : ${userReg.gender}</h4>
 	<br>
 	<h4>Email : ${userReg.communicationDTO.email}</h4>
+	<h4>Phone : ${userReg.communicationDTO.phone}</h4>
 </body>
 </html>
