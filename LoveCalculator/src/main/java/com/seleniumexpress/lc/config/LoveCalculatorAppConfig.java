86b6e3a41eb395841573849fe32ef5075e19cfd6
HarikaPenukonda <penukondaharika@gmail.com>
2025-01-21 16:52:18 -0600

    WebMvcConfigurer - for spring to recognize the formatter

--- LoveCalculator/src/main/java/com/seleniumexpress/lc/config/LoveCalculatorAppConfig.java
+++ LoveCalculator/src/main/java/com/seleniumexpress/lc/config/LoveCalculatorAppConfig.java
@@ -3,13 +3,17 @@ package com.seleniumexpress.lc.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.seleniumexpress.lc.formatter.PhoneNumberFormatter;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "com.seleniumexpress.lc.controller")
-public class LoveCalculatorAppConfig {
+public class LoveCalculatorAppConfig implements WebMvcConfigurer{
 	
 	@Bean
 	InternalResourceViewResolver viewResolver() {
@@ -19,6 +23,11 @@ public class LoveCalculatorAppConfig {
 		viewResolver.setSuffix(".jsp");
 		
 		return viewResolver;
+	}
+	
+	@Override
+	public void addFormatters(FormatterRegistry registry) {
+		registry.addFormatter(new PhoneNumberFormatter());
 	}
 
 }
