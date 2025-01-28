2c10785e9a26cfdfac5c97df1ea2119382cdb5c8
HarikaPenukonda <penukondaharika@gmail.com>
2025-01-28 13:49:38 -0600

    disallowing fields - username

--- LoveCalculator/src/main/java/com/seleniumexpress/lc/controller/RegistrationController.java
+++ LoveCalculator/src/main/java/com/seleniumexpress/lc/controller/RegistrationController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -54,8 +55,9 @@ public class RegistrationController {
 	}
 	
 	@InitBinder
-	public void initBinder() {
+	public void initBinder(WebDataBinder binder) {
 		System.out.println("<------inside the initBinder method------>");
+		binder.setDisallowedFields("userName");
 	}
 
 }
